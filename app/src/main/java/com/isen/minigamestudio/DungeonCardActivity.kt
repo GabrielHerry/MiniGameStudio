09f0e198d788f7c74a209a48a5f38eb53771fb78
inesd16 <ines.dobosz@hotmail.fr>
2020-02-17 08:50:47 +0100

    Merge branch 'master' of https://github.com/GabrielHerry/MiniGameStudio
    # Conflicts:
    #	app/src/main/java/com/isen/minigamestudio/DungeonCardActivity.kt

--- app/src/main/java/com/isen/minigamestudio/DungeonCardActivity.kt
+++ app/src/main/java/com/isen/minigamestudio/DungeonCardActivity.kt
@@ -2,11 +2,11 @@ package com.isen.minigamestudio
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_dungeon_card.*
+import kotlinx.android.synthetic.main.littleboxlayout.view.*
 
 
 class DungeonCardActivity : AppCompatActivity() {
@@ -16,8 +16,6 @@ class DungeonCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dungeon_card)
-
-
 
         dungeonAnim.pause()
 
@@ -46,85 +44,135 @@ class DungeonCardActivity : AppCompatActivity() {
 
         box1.setOnClickListener {
             if (checkAdjacentWithBox5(box1.boxPosition))
-                selectTheMove(establishMoveDirection(box1.boxPosition),R.id.box1)
+                selectTheMove(establishMoveDirection(box1.boxPosition), R.id.box1)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box2.setOnClickListener {
             if (checkAdjacentWithBox5(box2.boxPosition))
-                selectTheMove(establishMoveDirection(box2.boxPosition),R.id.box2)
+                selectTheMove(establishMoveDirection(box2.boxPosition), R.id.box2)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box3.setOnClickListener {
             if (checkAdjacentWithBox5(box3.boxPosition))
-                selectTheMove(establishMoveDirection(box3.boxPosition),R.id.box3)
+                selectTheMove(establishMoveDirection(box3.boxPosition), R.id.box3)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box4.setOnClickListener {
             if (checkAdjacentWithBox5(box4.boxPosition))
-                selectTheMove(establishMoveDirection(box4.boxPosition),R.id.box4)
+                selectTheMove(establishMoveDirection(box4.boxPosition), R.id.box4)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box5.setOnClickListener {
-            Toast.makeText(this,"box 5 pos : x =${box5.x}, y =${box5.y} !",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "box 5 pos : x =${box5.x}, y =${box5.y} !", Toast.LENGTH_SHORT)
+                .show()
         }
 
         box6.setOnClickListener {
             if (checkAdjacentWithBox5(box6.boxPosition))
-                selectTheMove(establishMoveDirection(box6.boxPosition),R.id.box6)
+                selectTheMove(establishMoveDirection(box6.boxPosition), R.id.box6)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box7.setOnClickListener {
             if (checkAdjacentWithBox5(box7.boxPosition))
-                selectTheMove(establishMoveDirection(box7.boxPosition),R.id.box7)
+                selectTheMove(establishMoveDirection(box7.boxPosition), R.id.box7)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box8.setOnClickListener {
             if (checkAdjacentWithBox5(box8.boxPosition))
-                selectTheMove(establishMoveDirection(box8.boxPosition),R.id.box8)
+                selectTheMove(establishMoveDirection(box8.boxPosition), R.id.box8)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
 
         box9.setOnClickListener {
             if (checkAdjacentWithBox5(box9.boxPosition))
-                selectTheMove(establishMoveDirection(box9.boxPosition),R.id.box9)
+                selectTheMove(establishMoveDirection(box9.boxPosition), R.id.box9)
             else
-                Toast.makeText(this,"Attention tu ne peux pas aller ici !",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Attention tu ne peux pas aller ici !",
+                    Toast.LENGTH_SHORT
+                ).show()
         }
-
 
 
     }
 
-    private fun checkAdjacentWithBox5 (clickBoxPos:Int) : Boolean {
+    private fun checkAdjacentWithBox5(clickBoxPos: Int): Boolean {
 
         when (box5.boxPosition) {
-            1 -> if (clickBoxPos == 2 || clickBoxPos == 4) { return true }
-            2 -> if (clickBoxPos == 1 || clickBoxPos == 3 || clickBoxPos == 5) { return true }
-            3 -> if (clickBoxPos == 2 || clickBoxPos == 6) { return true }
-            4 -> if (clickBoxPos == 1 || clickBoxPos == 5 || clickBoxPos == 7) { return true }
-            5 -> if (clickBoxPos == 2 || clickBoxPos == 4 || clickBoxPos == 6 || clickBoxPos == 8 ) { return true }
-            6 -> if (clickBoxPos == 3 || clickBoxPos == 5 || clickBoxPos == 9) { return true }
-            7 -> if (clickBoxPos == 4 || clickBoxPos == 8 ) { return true }
-            8 -> if (clickBoxPos == 7 || clickBoxPos == 5 || clickBoxPos == 9) { return true }
-            9 -> if (clickBoxPos == 6 || clickBoxPos == 8 ) { return true }
+            1 -> if (clickBoxPos == 2 || clickBoxPos == 4) {
+                return true
+            }
+            2 -> if (clickBoxPos == 1 || clickBoxPos == 3 || clickBoxPos == 5) {
+                return true
+            }
+            3 -> if (clickBoxPos == 2 || clickBoxPos == 6) {
+                return true
+            }
+            4 -> if (clickBoxPos == 1 || clickBoxPos == 5 || clickBoxPos == 7) {
+                return true
+            }
+            5 -> if (clickBoxPos == 2 || clickBoxPos == 4 || clickBoxPos == 6 || clickBoxPos == 8) {
+                return true
+            }
+            6 -> if (clickBoxPos == 3 || clickBoxPos == 5 || clickBoxPos == 9) {
+                return true
+            }
+            7 -> if (clickBoxPos == 4 || clickBoxPos == 8) {
+                return true
+            }
+            8 -> if (clickBoxPos == 7 || clickBoxPos == 5 || clickBoxPos == 9) {
+                return true
+            }
+            9 -> if (clickBoxPos == 6 || clickBoxPos == 8) {
+                return true
+            }
         }
         return false
     }
 
-    private fun establishMoveDirection (clickBoxPos:Int) : String {
+    private fun establishMoveDirection(clickBoxPos: Int): String {
         when (box5.boxPosition) {
             1 -> when (clickBoxPos) {
                 2 -> return "mvRight"
@@ -180,7 +228,7 @@ class DungeonCardActivity : AppCompatActivity() {
         return "Error"
     }
 
-    private fun selectTheMove (myMove: String, boxToMoveId: Int) {
+    private fun selectTheMove(myMove: String, boxToMoveId: Int) {
         when (myMove) {
             "mvTop" -> animMoveToTop(boxToMoveId)
             "mvLow" -> animMoveToLow(boxToMoveId)
@@ -191,18 +239,21 @@ class DungeonCardActivity : AppCompatActivity() {
     }
 
 
-
-
     private fun animMoveToTop(boxToMoveId: Int) {
 
         val littleBoxSize = box1.height.toFloat()
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "Y", (box5.y -littleBoxSize)).apply{
-            duration = 1000
-            start()
-        }
+        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "Y", (box5.y - littleBoxSize))
+            .apply {
+                duration = 1000
+                start()
+            }
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(boxToMoveId), "Y", (findViewById<LittleBox>(boxToMoveId).y + littleBoxSize)).apply{
+        ObjectAnimator.ofFloat(
+            findViewById<LittleBox>(boxToMoveId),
+            "Y",
+            (findViewById<LittleBox>(boxToMoveId).y + littleBoxSize)
+        ).apply {
             duration = 1000
             start()
         }
@@ -214,7 +265,7 @@ class DungeonCardActivity : AppCompatActivity() {
 
         box5.boxPosition = newPos
         box5.textBoxNumber.text = newPos.toString()
-      //  Toast.makeText(this,"fin du top",Toast.LENGTH_LONG).show()
+        //  Toast.makeText(this,"fin du top",Toast.LENGTH_LONG).show()
 
     }
 
@@ -222,12 +273,17 @@ class DungeonCardActivity : AppCompatActivity() {
 
         val littleBoxSize = box1.height.toFloat()
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "X", (box5.x + littleBoxSize)).apply{
-            duration = 1000
-            start()
-        }
+        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "X", (box5.x + littleBoxSize))
+            .apply {
+                duration = 1000
+                start()
+            }
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(boxToMoveId), "X",(findViewById<LittleBox>(boxToMoveId).x - littleBoxSize)).apply{
+        ObjectAnimator.ofFloat(
+            findViewById<LittleBox>(boxToMoveId),
+            "X",
+            (findViewById<LittleBox>(boxToMoveId).x - littleBoxSize)
+        ).apply {
             duration = 1000
             start()
         }
@@ -248,16 +304,20 @@ class DungeonCardActivity : AppCompatActivity() {
 
         val littleBoxSize = box1.height.toFloat()
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "X",(box5.x - littleBoxSize)).apply{
+        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "X", (box5.x - littleBoxSize))
+            .apply {
+                duration = 1000
+                start()
+            }
+
+        ObjectAnimator.ofFloat(
+            findViewById<LittleBox>(boxToMoveId),
+            "X",
+            (findViewById<LittleBox>(boxToMoveId).x + littleBoxSize)
+        ).apply {
             duration = 1000
             start()
         }
-
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(boxToMoveId), "X",(findViewById<LittleBox>(boxToMoveId).x + littleBoxSize)).apply{
-            duration = 1000
-            start()
-        }
-
 
 
         // change the position number of the two boxs
@@ -274,12 +334,17 @@ class DungeonCardActivity : AppCompatActivity() {
 
 
         val littleBoxSize = box1.height.toFloat()
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "Y",box5.y + littleBoxSize).apply{
-            duration = 1000
-            start()
-        }
+        ObjectAnimator.ofFloat(findViewById<LittleBox>(R.id.box5), "Y", box5.y + littleBoxSize)
+            .apply {
+                duration = 1000
+                start()
+            }
 
-        ObjectAnimator.ofFloat(findViewById<LittleBox>(boxToMoveId), "Y",(findViewById<LittleBox>(boxToMoveId).y -littleBoxSize)).apply{
+        ObjectAnimator.ofFloat(
+            findViewById<LittleBox>(boxToMoveId),
+            "Y",
+            (findViewById<LittleBox>(boxToMoveId).y - littleBoxSize)
+        ).apply {
             duration = 1000
             start()
         }
@@ -296,8 +361,6 @@ class DungeonCardActivity : AppCompatActivity() {
     }
 
 }
-
-
 
 
 //-------------- an other way but without a smooth transition
@@ -369,16 +432,6 @@ constraintSetArrival2.applyTo(mylayout)   */
 
 
 // Log.d("I click","click")
-
-
-
-
-
-
-
-
-
-
 
 
 /*
